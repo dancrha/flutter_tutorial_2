8cfcda53c121f477c02da3cd48c64fefa2740200
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-11-08 19:39:52 -0500

    progress

--- lib/main.dart
+++ lib/main.dart
@@ -3,38 +3,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({super.key});
+
+  List names = ['d', 'ttt'];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.deepPurple[200],
-        appBar: AppBar(
-          title: Text("App Bar"),
-          backgroundColor: Colors.deepPurple,
-          elevation: 0,
-          leading: Icon(Icons.menu),
-          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
-        ),
-        body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-                color: Colors.deepPurple,
-                borderRadius: BorderRadius.circular(20)),
-            padding: EdgeInsets.all(25),
-            child: Icon(
-              Icons.ac_unit,
-              color: Colors.white,
-              size: 28,
-            ),
+        body: ListView.builder(
+          itemCount: names.length,
+          itemBuilder: (context, index) => ListTile(
+            title: Text(names[index]),
           ),
         ),
       ),
